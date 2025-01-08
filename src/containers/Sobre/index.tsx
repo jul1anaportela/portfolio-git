17fe60194a4cdfd381036cc270750d7e448e1273
Juliana Portela <bazil.helpdesk@plan-international.org>
2025-01-08 17:51:05 -0300

    finalizando seção -sobre mim-

--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -1,5 +1,7 @@
 import Titulo from '../../components/Titulo'
 import Paragrafo from '../../components/Paragrafo'
+import { GithubSecao } from './styles'
+
 const Sobre = () => (
   <section>
     <Titulo fontSize={16}>Sobre mim</Titulo>
@@ -9,6 +11,10 @@ const Sobre = () => (
       explicabo harum odit animi sint cupiditate quas perspiciatis dignissimos
       illo ea ab.
     </Paragrafo>
+    <GithubSecao>
+      <img src="https://github-readme-stats.vercel.app/api?username=jul1anaportela&show_icons=true&theme=dracula&include_all_comits=true&count_private=true" />
+      <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=jul1anaportela&layout=compact&langs_count=7&theme=dracula" />
+    </GithubSecao>
   </section>
 )
 
