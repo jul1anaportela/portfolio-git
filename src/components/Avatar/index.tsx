eae4c367904babb92121a8e2e8532ceab49d0123
Juliana Portela <bazil.helpdesk@plan-international.org>
2025-01-08 18:56:59 -0300

    estilizando Sidebar

---
+++ src/components/Avatar/index.tsx
@@ -0,0 +1,5 @@
+import { FotoPerfil } from './styles'
+
+const Avatar = () => <FotoPerfil src="https://github.com/jul1anaportela.png" />
+
+export default Avatar
--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,22 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/jul1anaportela.png" />
-    <Titulo fontSize={20}>Juliana Portela</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={16}>Juliana Portela</Titulo>
+      <Paragrafo tipo="secundario" fontSize={14}>
+        jul1anaportela
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={10}>
+        Engenheira FrontEnd
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
