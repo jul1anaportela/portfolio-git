d6082a0a2f7362d4ff7031118641813ff1e1ab66
Juliana Portela <bazil.helpdesk@plan-international.org>
2025-01-08 16:29:40 -0300

    estrutura construída

---
+++ src/containers/Sidebar/index.tsx
@@ -0,0 +1,10 @@
+import Titulo from '../../components/Titulo'
+
+const Sidebar = () => (
+  <aside>
+    <img src="https://github.com/jul1anaportela.png" />
+    <Titulo>Juliana Portela</Titulo>
+  </aside>
+)
+
+export default Sidebar
---
+++ src/containers/Sobre/index.tsx
@@ -0,0 +1,15 @@
+import Titulo from '../../components/Titulo'
+
+const Sobre = () => (
+  <section>
+    <Titulo>Sobre mim</Titulo>
+    <p>
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi cum optio
+      molestias veritatis maiores, aspernatur asperiores mollitia reprehenderit
+      explicabo harum odit animi sint cupiditate quas perspiciatis dignissimos
+      illo ea ab.
+    </p>
+  </section>
+)
+
+export default Sobre
